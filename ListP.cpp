94490feeda111255e6750a61b5c6b7ae4cf3caba
ZanYip <zanyip94@gmail.com>
2016-07-22 17:48:24 +0800

    Update ListP.cpp

--- ListP.cpp
+++ ListP.cpp
@@ -13,3 +13,10 @@ void insertMiddle(char item, int Index){
     newitem->next = cur->next; //next address of node newitem will be pointing to the same address where cur is pointing to.
     cur->next = newitem; 
 }
+
+void insertLast(char item, int Index){
+    Nodeindex *newitem = newNodeindex;
+    newitem->Index = 2;
+    cur->next = newitem;
+    newitem->next = NULL;
+}
