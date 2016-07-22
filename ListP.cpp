6548250900a9e805a4afbf8ad23355260db7fd5b
ZanYip <zanyip94@gmail.com>
2016-07-22 23:23:30 +0800

    Update ListP.cpp

--- ListP.cpp
+++ ListP.cpp
@@ -1,25 +1,8 @@
 #include "ListP.h"
 
-void insertFirst(char item, int Index){
-    Nodeindex *newitem = newNodeindex;
-    newitem->Index = 2;
-    newitem->next = head;
-    head = newitem;
-}
+void insert(char item, int Index){
+    
 
-void insertMiddle(char item, int Index){
-    Nodeindex *newitem = newNodeindex;
-    newitem->Index = 2;
-    newitem->next = cur->next; //next address of node newitem will be pointing to the same address where cur is pointing to.
-    cur->next = newitem; 
-}
-
-void insertLast(char item, int Index){
-    Nodeindex *newitem = newNodeindex;
-    newitem->Index = 2;
-    cur->next = newitem;
-    newitem->next = NULL;
-}
 
 void delete(char item, int Index)
 {
