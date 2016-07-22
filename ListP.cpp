61d3c36ee5877ff0c719058de10f2d1fb6c759e0
qidongtee1995 <qidongtee1995@users.noreply.github.com>
2016-07-22 17:49:51 +0800

    Update ListP.cpp

--- ListP.cpp
+++ ListP.cpp
@@ -20,3 +20,20 @@ void insertLast(char item, int Index){
     cur->next = newitem;
     newitem->next = NULL;
 }
+
+void delete(char item, int Index)
+{
+    struct Node* temp1 = head;
+    if(n ==1){
+        head = temp1->next;
+        free(temp1);
+        return;
+    }
+    int i;
+    for(i = 0; i < n-2; i++)
+        temp1 = temp1->next;
+    
+    struct Node* temp2 = temp1->next;
+    temp1->next = temp2->next;
+    free(temp2);
+}
