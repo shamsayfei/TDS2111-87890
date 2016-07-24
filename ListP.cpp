0aacc65193700305b7c11fee08ff854737998622
tsueysiang <rovers_hell@hotmail.com>
2016-07-24 23:17:55 +0800

    Update ListP.cpp

--- ListP.cpp
+++ ListP.cpp
@@ -2,13 +2,13 @@
 
 void insert(char item, int Index){
     struct Node* newPtr = new Node();
-    if(n == 1){
+    if(Index == 1){
         newPtr->next = head;
         head = newPtr;
         return;
     }
     Node* newPtr2 = head;
-    for(int i=0; i<n-2; i++){
+    for(int i=0; i<Index-2; i++){
         newPtr2 = newPtr2->next;
     }
 newPtr->next = newPtr2->next;
@@ -17,14 +17,14 @@ newPtr2->next = newPtr;
 
 void delete(char item, int Index){
     struct Node* newPtr = head;
-    if(n ==1){
+    if(Index ==1){
         head = newPtr->next;
         free(newPtr);
         return;
     }
     int i;
-    for(i = 0; i < n-2; i++)
-        newPtr = newPtr->next; //newPtr pointing to (n-1)th node
+    for(i = 0; i < Index-2; i++)
+        newPtr = newPtr->next; //newPtr pointing to (Index-1)th node
     
     struct Node* newPtr2 = newPtr->next; //nth node
     newPtr->next = newPtr2->next;
