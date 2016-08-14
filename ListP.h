7f70ceca185a34f946a5d7c58ae4c6f63b938455
tsueysiang <rovers_hell@hotmail.com>
2016-08-14 16:30:21 +0800

    Update ListP.h

--- ListP.h
+++ ListP.h
@@ -5,23 +5,19 @@
 #include <string>
 using namespace std;
 
-class List{
-    
-private:
     struct node
     {
-        node * next;
-    };
         char item;
+        node * next;
         int Index;
+    };
 
-public:
     void insert(char item, int Index, string subtitle);
     void delete(char item, int Index);
     void remove(int Index);
     void remove(int Index, string subtitle);
     void remove(string subtitle);
     string retrieve(int Index);
-    bool find(int Index);
-}
+    find(int Index);
+
 #endif
