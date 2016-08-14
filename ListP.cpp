db56fb2110a1c7d80e06617dd7a05afdfba70850
shamsayfei <fei18112@yahoo.com.my>
2016-08-14 15:44:36 +0800

    Update ListP.cpp

--- ListP.cpp
+++ ListP.cpp
@@ -1,6 +1,6 @@
 #include "ListP.h"
 
-void insert(char item, int Index){
+void insert(char item, int Index, string subtitle){
     struct Node* newPtr = new Node();
     if(Index == 1){
         newPtr->next = head;
@@ -17,7 +17,7 @@ newPtr2->next = newPtr;
 
 void delete(char item, int Index){
     struct Node* newPtr = head;
-    if(Index ==1){
+    if(Index == 1){
         head = newPtr->next;
         delete newPtr;
         return;
@@ -29,4 +29,22 @@ void delete(char item, int Index){
     struct Node* newPtr2 = newPtr->next; //nth node
     newPtr->next = newPtr2->next;
     delete newPtr2;
+}
+
+void remove(int Index){
+    Node * prev;
+    if(Index<1){
+        return;
+    }
+    else if(Index == 1){
+        cur = head;
+        head = head->next;
+    }
+    else{
+        Node * prev = Index-1;
+        cur = prev->next;
+        prev->next = cur->next;
+    }
+    cur->next = NULL;
+    delete cur;
 }
